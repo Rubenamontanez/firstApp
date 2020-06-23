afff725f29f3024738a864025f882242d5bfa2d9
Ruben Montanez <ruben.a.montanez@gmail.com>
2020-06-23 15:11:41 -0500

    added more JS

--- js/scope.js
+++ js/scope.js
@@ -6,3 +6,29 @@ const logCitySkyline = () =>{
 };
 
 console.log(logCitySkyline());
+
+
+//global scope
+let satellite = 'The Moon';
+let galaxy = 'The Milky Way';
+let stars = 'North Star';
+
+const callMyNightSky = () =>{
+    stars = 'Sirius';
+    return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+}
+
+console.log(callMyNightSky());
+console.log(stars)
+
+// block scope
+const logVisibleLightWaves = () =>{
+    const lightWaves = 'Moonlight';
+    let region = 'The Artic';
+    if (region === 'The Arctic'){
+        let lightWaves = 'Northern Lights';
+        console.log(lightWaves);
+    }
+    console.log(lightWaves);
+}
+console.log(logVisibleLightWaves());
